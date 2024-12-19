ab80f0152e983919d757ac33881ae6a0a2d8f78a
ShadySoltan <shady.aly.soltan@gmail.com>
2024-12-19 18:03:24 +0200

    Update Colors and hovers

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -58,7 +58,7 @@ export default function Generator(props) {
                         <button onClick={() => {
                             setMuscles([])
                             setPoison(type)
-                        }} className={'bg-slate-950 border  duration-200 px-4 hover:border-blue-600 py-3 rounded-lg ' + (type === poison ? ' border-blue-600' : ' border-blue-400')} key={typeIndex}>
+                        }} className={'bg-slate-950 border  duration-200 px-4 transform transition-transform duration-300 hover:scale-105 hover:bg-blue-950 hover:border-blue-600 py-3 rounded-lg ' + (type === poison ? ' border-blue-600 bg-blue-950' : ' border-blue-400')} key={typeIndex}>
                             <p className='capitalize'>{type.replaceAll('_', " ")}</p>
                         </button>
                     )
@@ -90,7 +90,7 @@ export default function Generator(props) {
                     return (
                         <button onClick={() => {
                             setGoal(scheme)
-                        }} className={'bg-slate-950 border  duration-200 hover:border-blue-600 py-3 rounded-lg px-4 ' + (scheme === goal ? ' border-blue-600' : ' border-blue-400')} key={schemeIndex}>
+                        }} className={'bg-slate-950 border transform transition-transform duration-300 hover:scale-105 hover:bg-blue-950  duration-200 hover:border-blue-600 py-3 rounded-lg px-4 ' + (scheme === goal ? ' border-blue-600 bg-blue-950' : ' border-blue-400')} key={schemeIndex}>
                             <p className='capitalize'>{scheme.replaceAll('_', " ")}</p>
                         </button>
                     )
